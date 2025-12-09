52dc64e292f3d786fd728319e901c3acca6237c3
thomas.johanson <thomas.johanson@cctsglobal.com>
2025-12-09 15:15:22 +0530

    Initial Migrations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
